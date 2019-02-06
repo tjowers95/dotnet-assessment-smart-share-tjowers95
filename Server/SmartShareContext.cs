53605b9af25654cdd82cef0faafb5b5c065c344b
Quinton Bolt <qbolt@cooksys.com>
2019-02-06 10:08:25 -0600

    Changed schema name in connection string

--- Server/SmartShareContext.cs
+++ Server/SmartShareContext.cs
@@ -10,7 +10,7 @@ namespace CsharpAssessmentSmartShare
         // TODO define context and models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("server=127.0.0.1;port=5432;database=example;userid=postgres;password=bondstone");
+            optionsBuilder.UseNpgsql("server=127.0.0.1;port=5432;database=smartshare;userid=postgres;password=bondstone");
         }
     }
 
